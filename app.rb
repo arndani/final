d65725d30e907dad3d487286c1d4c4d731f270c0
arndani <dan.arnold328@gmail.com>
2020-03-10 03:10:59 +0000

    pushit

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ require "sinatra/reloader" if development?                                      
 require "sequel"                                                                      #
 require "logger"                                                                      #
 require "twilio-ruby"                                                                 #
-require "bcrypt"                                                                      #
+require "bcrypt"   
+require "geocoder"                                                                   #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
 DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
@@ -14,5 +15,53 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+wedding_table = DB.from(:Wedding_Event)
+hotel_table = DB.from(:hotel)
+
+
+get "/" do
+    puts "params: #{params}"
+
+    pp wedding_table.all.to_a
+    @wedding = wedding_table.all.to_a
+    view "Wedding_page"
+end
+
+
+get "/wedding/:id" do
+    puts "params: #{params}"
+
+    pp wedding_table.where(id: params[:id]).to_a[0]
+    @details = wedding_table.where(id: params[:id]).to_a[0]
+    @hotel=hotel_table.where(id: params[:id]).to_a[0]
+    if@details[:id] == 1 then
+        view "RSVP"
+    elsif @details[:id] == 2 then
+        view "Hotels"
+    else
+        view "search"
+    end
+end
+
+get "/wedding/map" do
+
+
+ @details = wedding_table.where(id: params[:id]).to_a[0]
+    @hotel=hotel_table.where(id: params[:id]).to_a[0]
+    
+
+    view "search"
+    
+end
+
+
+get "/wedding/map/view" do
+    
+    results = Geocoder.search(params["q"])
+    lat_long = results.first.coordinates # => [lat, long]
+      @lat = lat_long[0]
+  @long =  lat_long[1]
+  @lat_long = "#{@lat},#{@long}"
+    "#{lat_long[0]} #{lat_long[1]}"
+    view "where2"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,59 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :Wedding_Event do
   primary_key :id
   String :title
   String :description, text: true
   String :date
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :hotel do
   primary_key :id
-  foreign_key :event_id
+  foreign_key :hotel_id
+  String :description
+  String :hotel_name
+  String :website
+  String :location
+end
+
+DB.create_table! :user do
+  primary_key :id
+  foreign_key :user_id
   Boolean :going
   String :name
   String :email
-  String :comments, text: true
+  String :hotel
 end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+events_table = DB.from(:Wedding_Event)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(title: "RSVP", 
+                    description: "Please let us know if you are able to join us!",
+                    date: "September 5",
+                    location: "Rome, Italy")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
+events_table.insert(title: "Hotel Booking", 
+                    description: "We have 3 wonderful resort destinations to choose from.",
+                    date: "September 5",
                     location: "Nowhere")
+
+
+                    hotel_table = DB.from(:hotel)
+
+hotel_table.insert(hotel_name: "The Inn at the Spanish Steps", 
+                    description: "At the Inn at the Spanish Steps, boutique hospitality in a privileged location on the famed Via Condotti, is part of a new concept combining personalized services with a variety of luxury accommodation tailored to guest’s every desire. ",
+                    website: "https://www.theinnatthespanishsteps.com/?gclid=Cj0KCQjw0pfzBRCOARIsANi0g0vkVpU1sw0f1trqbbxpMFIe3pJibaT1evZ974-sZHBKtdygiQi1aW0aAs21EALw_wcB",
+                    location: "Via dei Condotti, 85, 00187 Roma RM, Italy")
+
+hotel_table.insert(hotel_name: "Hotel Hassler Roma", 
+                    description: "The Hassler Hotel is located in the heart of Rome's City Centre and is the perfect venue for luxury weddings, spa breaks, 5 star conferences, fine dining and more.",
+                   website: "https://www.hotelhasslerroma.com/",
+                    location: "Piazza della Trinità dei Monti, 6, 00187 Roma RM, Italy")
+
+hotel_table.insert(hotel_name: "Rome Marriott Grand Hotel Flora", 
+                    description: "Immerse in your personal Dolce Vita at the Rome Marriott Grand Hotel Flora, right on top of the noble Via Veneto and next door to the Villa Borghese gardens and the 2000 year old historic Roman walls.",
+                    website: "https://www.marriott.com/hotels/travel/romdt-rome-marriott-grand-hotel-flora/?scid=bb1a189a-fec3-4d19-a255-54ba596febe2",
+                    location: "Via Vittorio Veneto, 191, 00187 Roma RM, Italy")
+
